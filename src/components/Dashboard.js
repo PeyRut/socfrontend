0d7e9aa48bacb7fd3a3f90bf476e9c1387c54cad
PeyRut <pruthesell@icloud.com>
2025-01-14 11:43:40 -0600

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -41,7 +41,7 @@ const RightSection = styled.div`
   flex-direction: column;
   align-items: stretch;
   padding: 0;
-  margin-top: 50px; // or any value you like
+  margin-top: 57px; // or any value you like
   margin-left: 20px;
   border-left: none;
   height: 100%;
