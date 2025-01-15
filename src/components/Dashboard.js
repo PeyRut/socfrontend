f083db14491e466ff43a0d09625fef0043a50b7a
PeyRut <pruthesell@icloud.com>
2025-01-15 10:29:43 -0600

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -80,8 +80,8 @@ const WeekSelectorContainer = styled.div`
 */
 const ButtonGroup = styled.div`
   position: absolute;
-  left: -100px;  // Adjust horizontally
-  top: -10px;   // Adjust vertically
+  left: -200px;  // Adjust horizontally
+  top: 50px;   // Adjust vertically
 
   display: flex;
   gap: 10px;
